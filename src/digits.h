0bb270407d7d3182fd87dd093ae941ee4318872f
IvanGor <jaguarus34@gmail.com>
2022-03-01 18:42:42 +0300

    fix digits

--- src/digits.h
+++ src/digits.h
@@ -12,17 +12,12 @@ enum
 
 Lstrip *ledsDigits;
 Timer *t_digitsShow;
-Timer *t_ledsDigitsOff;
-Timer *t_ledsDigitsNextShow;
 
 void t_digitsShow_cb(void*);
 void digits_onActivate()
 {
     console->println(F("digits: onActivated"));
     t_digitsShow->launch(1,1);
-    // t_ledsDigitsNextShow->launch(DELAY_LEDS_NEXT_SHOW, Timer::TIMER_LOOP, [](void*){
-    //     *console << "t_ledsDigitsNextShow_cb" << endl;
-    // });
     digits_stage = DIGITS_STAGE_GAME;
     strcpy(props_states[DIGITS_STATE_POS], MQTT_STRSTATUS_ENABLED);
 }
@@ -30,6 +25,7 @@ void digits_onActivate()
 void digits_onFinish()
 {
     console->println(F("digits: onFinish"));
+    t_digitsShow->pause();
     digits_stage = DIGITS_STAGE_DONE;
     strcpy(props_states[DIGITS_STATE_POS], MQTT_STRSTATUS_FINISHED);
 }
@@ -67,23 +63,15 @@ void digits_init()
 {
     ledsDigits = new Lstrip(digits_ns::LSTRIPS_PIN, digits_ns::LSTRIPS_COUNT, digits_ns::RGB_ORDER);
     ledsDigits->set_segmentation(digits_ns::LSTRIPS_COUNT);
-    // ledsDigits->set_brightness(11);
     t_digitsShow = new Timer(t_digitsShow_cb);
-    t_ledsDigitsOff = new Timer();
-    t_ledsDigitsNextShow = new Timer();
 
     digits_stage = DIGITS_STAGE_NONE;
     strcpy(props_states[DIGITS_STATE_POS], MQTT_STRSTATUS_READY);
-
-    //debug
-    // digits_onActivate();
 }
 
 void digits_routine()
 {
     t_digitsShow->routine();
-    t_ledsDigitsOff->routine();
-    t_ledsDigitsNextShow->routine();
 
     if (digits_stage != DIGITS_STAGE_GAME)
         return;
