d44fe06f0fb9a3657a22ff5c2246fac6cdbdf80b
IvanGor <jaguarus34@gmail.com>
2022-03-02 18:43:51 +0300

    fix coin.h

--- src/coil.h
+++ src/coil.h
@@ -8,9 +8,12 @@ enum
     COIL_STAGE_DONE
 } coil_stage;
 
+SimpleLed *coin;
+
 void coil_onActivate()
 {
     console->println(F("coil: onActivated"));
+    coin->on();
     coil_stage = COIL_STAGE_GAME;
     strcpy(props_states[COIL_STATE_POS], MQTT_STRSTATUS_ENABLED);
 }
@@ -18,13 +21,14 @@ void coil_onActivate()
 void coil_onFinish()
 {
     console->println(F("coil: onFinish"));
+    coin->off();
     coil_stage = COIL_STAGE_DONE;
     strcpy(props_states[COIL_STATE_POS], MQTT_STRSTATUS_FINISHED);
 }
 
 void coil_init()
 {
-
+    coin = new SimpleLed(coil_ns::COIN_PIN);
     coil_stage = COIL_STAGE_NONE;
     strcpy(props_states[COIL_STATE_POS], MQTT_STRSTATUS_READY);
 }
